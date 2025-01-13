b243449955adfb8f096ccb789ab2e10132459bea
am-MongoDB <andrew.morgan@mongodb.com>
2025-01-13 17:49:44 +0000

    Add sponsors to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
 import FooterContact from './footer/FooterContact';
+import FooterSponsors from './footer/FooterSponsors';
 
 export default function Footer() {
 
   return (
     <div className='responsive-grid footer-grid' id="footer">
         <FooterContact/>
+        <FooterSponsors/>
     </div>
   );
 }
